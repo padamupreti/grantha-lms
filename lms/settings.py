85b88f645fd841e62e7f443b574bbd34c1ad8987
padamupreti <59172926+padamupreti@users.noreply.github.com>
2023-06-17 19:04:54 +0545

    Remove `pages` app from settings and urls

--- lms/settings.py
+++ lms/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'authentication.apps.AuthenticationConfig',
+    'dashboard.apps.DashboardConfig',
+    'qrmanager.apps.QrmanagerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig',
-    'dashboard.apps.DashboardConfig',
-    'qrmanager.apps.QrmanagerConfig',
 ]
 
 MIDDLEWARE = [
