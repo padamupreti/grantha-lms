28ffdc8124f72e8609431ce61e4b4dd07b571335
padamupreti <59172926+padamupreti@users.noreply.github.com>
2023-06-18 13:31:26 +0545

    Changed landing page redirect url

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
+@login_required(login_url='authentication:login-or-register')
 def home(request):
     return render(request, 'dashboard/home.html', {})
