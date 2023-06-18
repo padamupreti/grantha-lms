d6ad9c952ba8067af078a7c6e1d38b4bb3646104
padamupreti <59172926+padamupreti@users.noreply.github.com>
2023-06-18 12:47:56 +0545

    Started work on `dashboard` app

--- lms/urls.py
+++ lms/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('dashboard.urls')),
     path('auth/', include('authentication.urls')),
     path('admin/', admin.site.urls)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
