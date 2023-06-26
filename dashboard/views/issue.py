e442238d776a56a439499a5f3d05e47353df91a8
padamupreti <59172926+padamupreti@users.noreply.github.com>
2023-06-26 11:04:15 +0545

    Added Late fine creation mechanism for book return

--- dashboard/views/issue.py
+++ dashboard/views/issue.py
@@ -3,8 +3,11 @@ from django.contrib.auth.decorators import login_required
 
 from datetime import date
 
-from ..models import BookCopy, Issue
+from ..models import BookCopy, Issue, LateFine
 from ..forms.issue_forms import IssueCreateForm
+
+
+PER_DAY_FINE = 1
 
 
 @login_required
@@ -33,14 +36,36 @@ def list_issues(request):
 @login_required
 def return_issued_book(request, pk):
     issue = get_object_or_404(Issue, id=pk)
+    due_days = None
+    due_date = issue.due_date
+    today = date.today()
+    if due_date > today:
+        message = 'Book is not due for submission'
+    else:
+        due_days = abs(today - due_date).days
+        if due_days == 0:
+            message = 'Book is due for submission today'
+        else:
+            fine_amount = due_days * PER_DAY_FINE
+            message = f'Book is late for submission by {due_days} days. Late fine amount: Rupees {fine_amount}'
     context = {
         'book': issue.book_copy.book,
         'member': issue.member,
+        'message': message,
+        'due_days': due_days
     }
     if request.method == 'POST':
+        if due_days:
+            LateFine.objects.create(
+                book_copy=issue.book_copy,
+                member=issue.member,
+                late_days=due_days,
+                amount=fine_amount,
+                fined_date=today
+            )
         issue.book_copy.is_available = True
         issue.book_copy.save()
-        issue.returned_date = date.today()
+        issue.returned_date = today
         issue.save()
         return redirect('dashboard:list-issues')
     return render(request, 'dashboard/return_issued.html', context)
