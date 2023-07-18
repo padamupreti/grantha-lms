76be852376d515e8caacf231d7b639f8b9de0a78
padamupreti <59172926+padamupreti@users.noreply.github.com>
2023-07-18 23:13:49 +0545

    Added librarian and member dashboards

--- dashboard/views/general.py
+++ dashboard/views/general.py
@@ -3,10 +3,26 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views import View
 from django.contrib.auth.decorators import login_required
 
+from ..models import Author, Publisher, Category, Book, Issue, Request
+
 
 @login_required(login_url='authentication:login-or-register')
 def home(request):
-    return render(request, 'dashboard/home.html', {})
+    books = Book.objects.count()
+    if request.user.is_librarian:
+        context = {
+            'books': books,
+            'issues': Issue.objects.count(),
+            'book_requests': Request.objects.filter(is_fulfilled=False).count()
+        }
+    else:
+        context = {
+            'books': books,
+            'authors': Author.objects.count(),
+            'publishers': Publisher.objects.count(),
+            'categories': Category.objects.count()
+        }
+    return render(request, 'dashboard/home.html', context)
 
 
 class LibrarianView(UserPassesTestMixin, View):
